02b3af8b5baa4d46f0994062769e7353697cbcd2
Andre Richter <arichter@annova.de>
2020-03-27 17:11:54 +0100

    Added, a string for the message content for the SendMessageToClient method.

--- DasBessereDiscord2.0/App_Code/Service.cs
+++ DasBessereDiscord2.0/App_Code/Service.cs
@@ -9,7 +9,7 @@ using System.ServiceModel.Web;
 using System.Text;
 
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
-public class Service : IService
+public class Service : IService 
 {
 	public string GetData(int value)
 	{
@@ -73,7 +73,7 @@ public class Service : IService
 
     public void ServerGetMessageFromClient(string clientMessage)
     {
-        this.clientMessagecontent = clientMessage;
+        clientMessagecontent = clientMessage;
 
         IsClientMessageNull(clientMessage);
     }
