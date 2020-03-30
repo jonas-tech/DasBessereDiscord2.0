7a83b3dba031819220e98b3d2a058ba230444f5a
jonas.frackmann@gmx.de <jonas.frackmann@gmx.de>
2020-03-30 11:28:13 +0200

    Updated interfaces

--- DasBessereDiscord2.0/App_Code/IService.cs
+++ DasBessereDiscord2.0/App_Code/IService.cs
@@ -8,47 +8,25 @@ using System.Text;
 
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
 [ServiceContract]
+public interface IServiceClient
+{
+	[OperationContract(IsOneWay = true)]
+	string ServerSendMessageToClient();
+}
+[ServiceContract(CallbackContract = typeof(IServiceClient))]
+
+
 public interface IService
 {
-
-	[OperationContract]
-	string GetData(int value);
-
-	[OperationContract]
-	CompositeType GetDataUsingDataContract(CompositeType composite);
-
 	// TODO: Add your service operations here
-	[OperationContract]
-	void ClientLogIntoServer(bool ClientisLoggedin);
+	[OperationContract(IsOneWay =true)]
+	void ClientLogIntoServer(string username);
 
-	[OperationContract]
-	string ServerSendMessageToClient();
 
-	[OperationContract]
+	[OperationContract(IsOneWay =true)]
 	void ServerGetMessageFromClient(string clientMessage);
 
-	[OperationContract]
-	void ClientLogOutOfServer(bool ClientisLoggedin, int clientNumber);
+	[OperationContract(IsOneWay =true)]
+	void ClientLogOutOfServer(string username);
 }
 
-// Use a data contract as illustrated in the sample below to add composite types to service operations.
-[DataContract]
-public class CompositeType
-{
-	bool boolValue = true;
-	string stringValue = "Hello ";
-
-	[DataMember]
-	public bool BoolValue
-	{
-		get { return boolValue; }
-		set { boolValue = value; }
-	}
-
-	[DataMember]
-	public string StringValue
-	{
-		get { return stringValue; }
-		set { stringValue = value; }
-	}
-}
