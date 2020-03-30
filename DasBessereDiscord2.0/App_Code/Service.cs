075e2dea9420f8b189b8da58e61c389eb0379ad9
Andre Richter <arichter@annova.de>
2020-03-30 12:15:32 +0200

    Added, the clientUsername Dictionary.

--- DasBessereDiscord2.0/App_Code/Service.cs
+++ DasBessereDiscord2.0/App_Code/Service.cs
@@ -1,69 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+[ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.Single)]
 public class Service : IService
 {
-	public string GetData(int value)
-	{
-		return string.Format("You entered: {0}", value);
-	}
-
-	public CompositeType GetDataUsingDataContract(CompositeType composite)
-	{
-		if (composite == null)
-		{
-			throw new ArgumentNullException("composite");
-		}
-		if (composite.BoolValue)
-		{
-			composite.StringValue += "Suffix";
-		}
-		return composite;
-	}
-    private List<Socket> clientSockets = new List<Socket>();
-
-    Socket chatSocket;
-
-    Socket clientSocket;
-
-    IPHostEntry chatIPhost;
-
-    IPAddress clientIPadress;
-
-    IPEndPoint clientIPendPoint;
-
-    int amountOfclients;
+    Dictionary<IServiceClient, string> clientUsernames = new Dictionary<IServiceClient, string>();
 
     string clientMessagecontent = "";
 
-    public void ClientLogIntoServer(bool ClientisLoggedin)
+    public void ClientLogIntoServer(string userName)
     {
-        if (ClientisLoggedin == true)
-        {
-            chatIPhost = Dns.GetHostEntry("");
 
-            clientIPadress = chatIPhost.AddressList[0];
-
-            clientIPendPoint = new IPEndPoint(clientIPadress, 53225);
-
-            chatSocket.Bind(clientIPendPoint);
-
-            chatSocket.Listen(2);
-
-            clientSocket.Connect(clientIPendPoint);
-
-            clientSockets.Add(clientSocket);
-
-            amountOfclients++;
-        }
     }
 
     public string ServerSendMessageToClient()
@@ -78,16 +31,8 @@ public class Service : IService
         IsClientMessageNull(clientMessage);
     }
 
-    public void ClientLogOutOfServer(bool ClientisLoggedin, int clientNumber)
+    public void ClientLogOutOfServer(string userName)
     {
-        if (ClientisLoggedin == false)
-        {
-            clientSockets[clientNumber].Close();
-
-            clientSockets.Remove(clientSockets[clientNumber]);
-
-            amountOfclients--;
-        }
     }
 
     private void IsClientMessageNull(string clientMessage)
